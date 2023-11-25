227df41161c1610190dd0754cd82874137132115
Walid Zeggane <120360954+zegganewalid@users.noreply.github.com>
2023-11-25 21:47:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     let quizForm = document.getElementById('quiz-form');
     let alertDiv = document.getElementById('alert');
-    let allCorrect = false; // Variable pour suivre si toutes les réponses sont correctes
+    let allCorrect = false; 
 
-    // Vérifiez s'il y a déjà eu une réussite précédente
     let hasPreviousSuccess = localStorage.getItem('quizSuccess') === 'true';
     if (hasPreviousSuccess) {
         congratulationMessage();
     }
 
     quizForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire
-        checkAnswers(); // Appel de la fonction pour vérifier les réponses
+        event.preventDefault();
+        checkAnswers(); 
     });
 
     function checkAnswers() {
         let questions = document.querySelectorAll('.question-item');
         resetAnswerStyles(questions);
-        let currentAllCorrect = true; // Variable pour suivre si toutes les réponses sont correctes pour cette soumission
+        let currentAllCorrect = true; 
 
         questions.forEach(function (question) {
             let correctAnswer = question.querySelector('.answer[value="true"]');
@@ -32,12 +31,10 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
 
-        // Comparer l'état actuel avec l'état précédent
         if (currentAllCorrect && !allCorrect) {
             allCorrect = true;
             congratulationMessage();
         } else if (!currentAllCorrect && allCorrect) {
-            // L'utilisateur a répondu correctement précédemment, mais la réponse actuelle est incorrecte
             allCorrect = false;
             hideCongratulationsMessage();
         }
@@ -58,12 +55,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function congratulationMessage() {
-        // Affichez le message de félicitations seulement si toutes les réponses sont correctes
         if (allCorrect) {
             alertDiv.style.display = 'block';
             localStorage.setItem('quizSuccess', 'true');
             
-            // Masquer le message après 1.3 secondes
             setTimeout(function() {
                 hideCongratulationsMessage();
             }, 1300);
@@ -75,3 +70,10 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.removeItem('quizSuccess');
     }
 });
+
+
+
+
+
+
+
